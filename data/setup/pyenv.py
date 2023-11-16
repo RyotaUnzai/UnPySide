2e36cef012b276a059cf3409fc0bac5d02fe70c4
UnPySide <satellitear@gmail.com>
2023-11-16 17:07:52 +0900

    wip: temporary save of ongoing analysis data
    Temporarily saving the current state of analysis data for backup purposes. This commit will be updated once the analysis is further refined.

--- data/setup/pyenv.py
+++ data/setup/pyenv.py
@@ -4,13 +4,13 @@ import subprocess
 import threading
 from pathlib import Path
 from typing import Self
+from logging import Logger
 
 from settings import global_settings
-from software import AbstractSoftware
 from utils import Guard
 
 
-class Pyenv(AbstractSoftware):
+class Pyenv:
     """A Singleton class that represents a Pyenv instance.
     """
     __lock = threading.Lock()
@@ -23,7 +23,8 @@ class Pyenv(AbstractSoftware):
 
         return cls._instance
 
-    def __init__(self) -> Self:
+    def __init__(self,  logger: Logger) -> Self:
+        self.__logger = logger
         self.__settings = global_settings
         settings = self.__settings.pyenv
         self.__install_dir = install_dir = settings.install_dir
@@ -44,27 +45,27 @@ class Pyenv(AbstractSoftware):
 
         install_dir = self.__install_dir
 
-        print(f"Installing pyenv in '{install_dir}")
+        self.__logger.info(f"Installing pyenv in '{install_dir}")
 
         # pyenv-win のインストール
         subprocess.run(
             f'"{self.__settings.unpysidepy.interpreter_path}" -m pip install pyenv-win=={self.__version} --target "{self.__install_dir}'
         )
 
-        print(f"Installed pyenv in '{install_dir}")
+        self.__logger.info(f"Installed pyenv in '{install_dir}")
 
     def uninstall(self) -> None:
         install_dir = self.__install_dir
 
         if not install_dir.exists():
-            print(f"Skipped uninstall pyenv because not installed '{install_dir}'")
+            self.__logger.info(f"Skipped uninstall pyenv because not installed '{install_dir}'")
             return
 
-        print(f"Uninstalling pyenv from '{install_dir}'")
+        self.__logger.info(f"Uninstalling pyenv from '{install_dir}'")
 
         shutil.rmtree(install_dir)
 
-        print(f"Uninstalled pyenv from '{install_dir}'")
+        self.__logger.info(f"Uninstalled pyenv from '{install_dir}'")
 
     def apply_environ(self, environ: dict[str, str] | None = None) -> dict[str, str]:
         assert self.installed
@@ -83,13 +84,13 @@ class Pyenv(AbstractSoftware):
         assert self.installed
         Guard.is_not_none_or_whitespace(py_ver+"  ")
 
-        print(f"Installing Python {py_ver} into pyenv")
+        self.__logger.info(f"Installing Python {py_ver} into pyenv")
 
         env = self.apply_environ()
 
         subprocess.run(f'"{self.__pyenv_bat}" install {py_ver}', env=env)
 
-        print(f"Installed Python {py_ver} into pyenv")
+        self.__logger.info(f"Installed Python {py_ver} into pyenv")
 
     def uninstall_python(self, py_ver: str) -> None:
         # 指定バージョンの Python を pyenv からアンインストール
@@ -97,13 +98,13 @@ class Pyenv(AbstractSoftware):
         assert self.installed
         Guard.is_not_none_or_whitespace(py_ver)
 
-        print(f"Uninstalling Python {py_ver} from pyenv")
+        self.__logger.info(f"Uninstalling Python {py_ver} from pyenv")
 
         env = self.apply_environ()
 
         subprocess.run(f'"{self.__pyenv_bat}" uninstall {py_ver}', env=env)
 
-        print(f"Uninstalled Python {py_ver} from pyenv")
+        self.__logger.info(f"Uninstalled Python {py_ver} from pyenv")
 
     def create_env(self, target_dir: Path, py_ver: str) -> None:
         assert self.installed
