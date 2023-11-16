509b8634eb99a972140e7855678c047c89567c55
UnPySide <satellitear@gmail.com>
2023-11-16 17:09:46 +0900

    wip: temporary save of ongoing analysis data
    Temporarily saving the current state of analysis data for backup purposes. This commit will be updated once the analysis is further refined.

--- data/setup/setup.py
+++ data/setup/setup.py
@@ -1,17 +1,27 @@
 from pathlib import Path
 from typing import Final
 
+from logging import getLogger, basicConfig
+from rich.logging import RichHandler
+
 from pyenv import Pyenv
 from vscode import VSCode
-BASE_DIR: Final[Path] = Path(__file__).parent
+
+BASE_DIR: Final[Path] = Path(__file__).parents
 
 
 if __name__ == "__main__":
-    vscode = VSCode()
-    pyenv = Pyenv()
+    FORMAT = "%(message)s"
+    basicConfig(
+        level="NOTSET", format=FORMAT, datefmt="[%X]", handlers=[RichHandler()]
+    )
+    logger = getLogger("rich")
 
+    vscode = VSCode(logger)
     vscode.uninstall()
-    pyenv.uninstall()
-
     vscode.install()
+
+    pyenv = Pyenv(logger)
+    pyenv.uninstall()
     pyenv.install()
+
