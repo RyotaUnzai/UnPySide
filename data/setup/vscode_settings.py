96b2a834628009e8b2e2f9c1256ccc743b7191b2
UnPySide <satellitear@gmail.com>
2023-11-16 09:43:57 +0900

    wip: temporary save of ongoing analysis data
    Temporarily saving the current state of analysis data for backup purposes. This commit will be updated once the analysis is further refined.

--- data/setup/vscode_settings.py
+++ data/setup/vscode_settings.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 from pydantic import Field
 
-from data.setup.config import AbstractSoftWareConfig
+from config import AbstractSoftWareConfig
 
 
 class VSCodeConfig(AbstractSoftWareConfig):
@@ -12,16 +12,36 @@ class VSCodeConfig(AbstractSoftWareConfig):
 
     @property
     def installer_file_name(self) -> str:
+        """_summary_
+
+        Returns:
+            str: _description_
+        """
         return f"{self._installer_file_name}{self.version}.zip"
 
     @property
     def data_dir(self) -> Path:
+        """_summary_
+
+        Returns:
+            Path: _description_
+        """
         return self.install_dir / "data"
 
     @property
     def code_cmd_path(self) -> Path:
+        """Code 
+
+        Returns:
+            Path: _description_
+        """
         return self.install_dir / "bin\\code.cmd"
 
     @property
     def user_settings_path(self) -> Path:
+        """_summary_
+
+        Returns:
+            Path: _description_
+        """
         return self.install_dir / "data\\user-data\\User\\settings.json"
