25a12b61a017059113a628d3fbfa23d0e7bb1882
UnPySide <satellitear@gmail.com>
2023-11-16 09:21:59 +0900

    wip: temporary save of ongoing analysis data
    Temporarily saving the current state of analysis data for backup purposes. This commit will be updated once the analysis is further refined.

--- data/setup/pyenv.py
+++ data/setup/pyenv.py
@@ -1,7 +1,13 @@
+import os
+import shutil
+import subprocess
 import threading
+from pathlib import Path
 from typing import Self
 
+from settings import global_settings
 from software import AbstractSoftware
+from utils import Guard
 
 
 class Pyenv(AbstractSoftware):
@@ -14,4 +20,109 @@ class Pyenv(AbstractSoftware):
             with cls.__lock:
                 if not hasattr(cls, "_instance"):
                     cls._instance = super(Pyenv, cls).__new__(cls)
+
         return cls._instance
+
+    def __init__(self) -> Self:
+        self.__settings = global_settings
+        settings = self.__settings.pyenv
+        self.__install_dir = install_dir = settings.install_dir
+
+        self.__version = Guard.is_not_none_or_whitespace(settings.version)
+        self.__pywin_dir = install_dir / "pyenv-win"
+        self.__bin_dir = self.__pywin_dir / "bin"
+        self.__shims_dir = self.__pywin_dir / "shims"
+        self.__pyenv_bat = self.__bin_dir / "pyenv.bat"
+        self.__python_bat = self.__shims_dir / "python.bat"
+
+    @property
+    def installed(self) -> bool:
+        return self.__install_dir.exists()
+
+    def install(self) -> None:
+        assert not self.installed
+
+        install_dir = self.__install_dir
+
+        print(f"Installing pyenv in '{install_dir}")
+
+        # pyenv-win のインストール
+        subprocess.run(
+            f'"{self.__settings.unpysidepy.interpreter_path}" -m pip install pyenv-win=={self.__version} --target "{self.__install_dir}'
+        )
+
+        print(f"Installed pyenv in '{install_dir}")
+
+    def uninstall(self) -> None:
+        install_dir = self.__install_dir
+
+        if not install_dir.exists():
+            print(f"Skipped uninstall pyenv because not installed '{install_dir}'")
+            return
+
+        print(f"Uninstalling pyenv from '{install_dir}'")
+
+        shutil.rmtree(install_dir)
+
+        print(f"Uninstalled pyenv from '{install_dir}'")
+
+    def apply_environ(self, environ: dict[str, str] | None = None) -> dict[str, str]:
+        assert self.installed
+
+        if environ is None:
+            environ = os.environ.copy()
+
+        environ["PYENV"] = environ["PYENV_ROOT"] = environ["PYENV_HOME"] = str(self.__pywin_dir)
+        environ["PATH"] = str(self.__bin_dir) + ";" + str(self.__shims_dir) + ";" + environ["PATH"]
+
+        return environ
+
+    def install_python(self, py_ver: str) -> None:
+        # 指定バージョンの Python を pyenv にインストール
+
+        assert self.installed
+        Guard.is_not_none_or_whitespace(py_ver+"  ")
+
+        print(f"Installing Python {py_ver} into pyenv")
+
+        env = self.apply_environ()
+
+        subprocess.run(f'"{self.__pyenv_bat}" install {py_ver}', env=env)
+
+        print(f"Installed Python {py_ver} into pyenv")
+
+    def uninstall_python(self, py_ver: str) -> None:
+        # 指定バージョンの Python を pyenv からアンインストール
+
+        assert self.installed
+        Guard.is_not_none_or_whitespace(py_ver)
+
+        print(f"Uninstalling Python {py_ver} from pyenv")
+
+        env = self.apply_environ()
+
+        subprocess.run(f'"{self.__pyenv_bat}" uninstall {py_ver}', env=env)
+
+        print(f"Uninstalled Python {py_ver} from pyenv")
+
+    def create_env(self, target_dir: Path, py_ver: str) -> None:
+        assert self.installed
+        assert target_dir.is_absolute()
+        Guard.is_not_none_or_whitespace(py_ver)
+
+        self.install_python(py_ver)
+
+        env = self.apply_environ()
+        prev_cwd = os.getcwd()
+        os.chdir(target_dir)
+        try:
+            subprocess.run(f'"{self.__pyenv_bat}" local {py_ver}', env=env)
+
+            # create venv
+            subprocess.run(f'"{str(self.__python_bat)}" -B -m venv .venv', env=env)
+
+            # Disable warnings to update pip for use in virtual environments
+            with open(target_dir / ".venv" / "pip.ini", "w", encoding="utf-8") as f:
+                f.write("[global]\ndisable-pip-version-check = True")
+        finally:
+            os.chdir(prev_cwd)
--- data/setup/vscode.py
+++ data/setup/vscode.py
@@ -1,17 +1,126 @@
+import os
+import shutil
+import subprocess
 import threading
-from typing import Self
+from pathlib import Path
+from typing import Final
 
-from software import AbstractSoftware
+from cache import download_installer
+from settings import global_settings
+from software import AbstractSoftware, Singleton
+from utils import copy_file
+from vscode_settings import VSCodeConfig
+
+BASE_DIR: Final[Path] = Path(__file__).parent
+INCLUDE_DIR: Final[Path] = BASE_DIR.with_name("include")
 
 
-class VSCode(AbstractSoftware):
+class VSCode(AbstractSoftware, metaclass=Singleton):
     """A Singleton class that represents a VSCode instance.
     """
     __lock = threading.Lock()
 
-    def __new__(cls, *args, **kwargs) -> Self:
-        if not hasattr(cls, "_instance"):
-            with cls.__lock:
-                if not hasattr(cls, "_instance"):
-                    cls._instance = super(VSCode, cls).__new__(cls)
-        return cls._instance
+    def __init__(self) -> None:
+        self.__settings = global_settings.vscode
+
+    @property
+    def installed(self) -> bool:
+        return self.__settings.install_dir.exists()
+
+    def install(self) -> None:
+        print("Starting setup for Visual Studio Code")
+        assert not self.installed
+
+        print(
+            f"""Targets:
+    Installer: {self.installer_file_name}
+    Install path: {self.install_dir}
+    Extensions
+        """
+        )
+        for install_ext_file_name in self.__settings.install_extensions:
+            print(f"    {install_ext_file_name}")
+        print("")
+
+        self.__install_vscode()
+        self.__enable_portable_mode()
+        self.__set_env()
+        self.__install_extension()
+        self.__uninstall_extension()
+        self.__copy_user_setting()
+
+        print("Completed setup for Visual Studio Code")
+
+    def uninstall(self) -> None:
+        install_dir = self.__settings.install_dir
+        if install_dir.exists():
+            shutil.rmtree(install_dir)
+
+    @property
+    def settings(self) -> VSCodeConfig:
+        """_summary_
+
+        Returns:
+            VSCodeConfig: _description_
+        """
+        return self.__settings
+
+    @property
+    def installer_file_name(self) -> str:
+        return self.__settings.installer_file_name
+
+    @property
+    def install_dir(self) -> Path:
+        return self.__settings.install_dir
+
+    @property
+    def code_cmd(self) -> Path:
+        return self.__settings.code_cmd_path
+
+    def __install_vscode(self) -> None:
+        """Install VSCode
+        """
+        print("Installing Visual Studio Code ...")
+        installer_path = download_installer(self.installer_file_name, indent_count=1)
+        shutil.unpack_archive(installer_path, self.install_dir)
+        print(f"Installed Visual Studio Code to '{self.install_dir}'")
+
+    def __enable_portable_mode(self) -> None:
+        """Enable VSCode portable mode
+
+        url: https://code.visualstudio.com/docs/editor/portable#_enable-portable-mode
+        """
+        print("Enable VSCode portable mode")
+        data_dir = self.__settings.data_dir
+        os.makedirs(data_dir)
+        print(f"  Created '{data_dir}'")
+
+    def __set_env(self):
+        """Set Environment Variables
+
+        Note: (node:8824) [DEP0005] DeprecationWarning: Buffer() is deprecated due to security and usability issues.
+                Please use the Buffer.alloc(), Buffer.allocUnsafe(), or Buffer.from() methods instead.
+        """
+        self.__env = os.environ.copy()
+        self.__env["NODE_OPTIONS"] = "--no-deprecation"
+
+    def __install_extension(self) -> None:
+        """Install extension
+        """
+        for extension in self.__settings.install_extensions:
+            subprocess.run(f'"{self.code_cmd}" --install-extension "{extension}" --force', env=self.__env)
+
+    def __uninstall_extension(self) -> None:
+        """Uninstall extension
+        """
+        print("Uninstall extensions")
+        for uninstall_ext_id in self.__settings.uninstall_extensions:
+            subprocess.run(f'"{self.code_cmd}" --uninstall-extension "{uninstall_ext_id}"', env=self.__env)
+
+    def __copy_user_setting(self) -> None:
+        """Coping settings.json
+        """
+        src_path = INCLUDE_DIR / "vscode-user-settings"
+        dst_path = self.__settings.user_settings_path
+        print("Create global settings.json")
+        copy_file(src_path, dst_path, indent_count=1)
