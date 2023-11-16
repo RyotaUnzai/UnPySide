ffeb34f7d2c530309dea5ab53cf706f2ace4927f
UnPySide <satellitear@gmail.com>
2023-11-16 17:08:21 +0900

    wip: temporary save of ongoing analysis data
    Temporarily saving the current state of analysis data for backup purposes. This commit will be updated once the analysis is further refined.

--- data/setup/pyenv_settings.py
+++ data/setup/pyenv_settings.py
@@ -1,5 +1,10 @@
+from pathlib import Path
+from pydantic import Field
 from config import AbstractConfig
 
 
 class PyenvConfig(AbstractConfig):
-    ...
+    version: str = Field(alias="Version")
+    """software version"""
+    install_dir: Path = Field(alias="InstallDir")
+    """installation directory of the software"""
