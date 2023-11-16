81411e261fbfd000d28cd1c1d3a2f379e08296e5
UnPySide <satellitear@gmail.com>
2023-11-16 17:07:06 +0900

    wip: temporary save of ongoing analysis data
    Temporarily saving the current state of analysis data for backup purposes. This commit will be updated once the analysis is further refined.

--- data/setup/cache.py
+++ data/setup/cache.py
@@ -7,38 +7,35 @@ from settings import global_settings
 
 
 def download_installer(filename: Union[str, Path], indent_count: int = 0) -> Path:
-    """指定されたファイルをダウンロードしてキャッシュし、キャッシュされたファイルの完全パスを返します。
-    既にキャッシュされているときは新たにダウンロードしません。
-    完全パスが渡されたときは渡されたパスを何もせずそのまま返します。
+    """Downloads and caches the specified file, returning the full path to the cached file.
+
+    If the file is already cached, it will not be downloaded again.
+    If a full path is provided, it simply returns the given path without any action.
 
     Args:
-        filename: ダウンロードするファイルの名前または相対パス。
-        indent_count: ログのインデント。
+        filename : The filename or path of the file to download.
+        indent_count : The level of indentation for the output. Default is 0.
 
     Returns:
-        Path: キャッシュファイルの完全パス。
+        Path: The full path to the downloaded (or already cached) file.
+
     """
     if isinstance(filename, str):
         filename = Path(filename)
 
-    # 完全パスなら何もしないでそのまま返す
     if filename.is_absolute():
         return filename
 
     indent = "    " * indent_count 
 
-    # キャッシュファイルパス
     cache_file = global_settings.dev_cache_dir / filename
 
-    # すでにキャッシュに存在すればそのまま返す
     if cache_file.exists():
         print(f"{indent}Already exists in cache '{cache_file}'")
         return cache_file
-    
-    # キャッシュフォルダー作成
+
     os.makedirs(global_settings.dev_cache_dir, exist_ok=True)
 
-    # ダウンロード（コピー）
     src_path = global_settings.installer_dir / filename
     print(f"{indent}Downloading {src_path}")
     shutil.copyfile(src_path, cache_file)
